610842d9c7c542d41b0b7436aa7ea8a593f6c9c0
brayan <bryancris03@gmail.com>
2026-05-27 23:14:21 -0500

    feat: implementacion de noticias

---
+++ StreamGo-backend/src/main/java/com/StreamGo/repository/NoticiaRepository.java
@@ -0,0 +1,13 @@
+package com.StreamGo.repository;
+
+import com.StreamGo.entity.Noticia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
+
+    List<Noticia> findByAutorId(Long idAutor);
+
+    List<Noticia> findByUsuarioId(Long idUsuario);
+}
