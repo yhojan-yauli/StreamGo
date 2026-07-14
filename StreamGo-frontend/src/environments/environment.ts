972fe8022a26a62d3bd6fc05cd4824e4381404c4
Artlyse.dev <franxrooswvelt@gmail.com>
2026-07-14 11:50:33 -0500

    Update fileServerUrl in environment configuration

--- StreamGo-frontend/src/environments/environment.ts
+++ StreamGo-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://streamgo-backend-3ex8.onrender.com'
-  fileServerUrl: 'https://portsmouth-think-integrity-exhibits.trycloudflare.com'
+  fileServerUrl: 'https://towards-merit-payroll-mods.trycloudflare.com'
 };
