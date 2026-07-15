38fda0ecf0cbae8a21d9f3051bfc961ff1be4a9b
Levi-EG <leviegty@gmail.com>
2026-07-15 08:18:34 -0500

    Add method to find ContenidoVotable by votes descending

--- StreamGo-backend/src/main/java/com/StreamGo/repository/ContenidoVotableRepository.java
+++ StreamGo-backend/src/main/java/com/StreamGo/repository/ContenidoVotableRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ContenidoVotableRepository extends JpaRepository<ContenidoVotable, Long> {
+
     List<ContenidoVotable> findByActivoTrue();
+    
+    List<ContenidoVotable> findAllByOrderByCantidadVotosDesc();
 }
